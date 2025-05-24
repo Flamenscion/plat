3520f54277329f2a5bcb341ba4ca0bb2f7d53a61
Metra <metra-2004@mail.ru>
2025-05-24 15:27:55 +0500

    first commit

---
+++ src/main/java/com/site/plat/PlatApplication.java
@@ -0,0 +1,12 @@
+package com.site.plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatApplication.class, args);
+	}
+}
